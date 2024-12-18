a6edbcc91ce51390ab9666430c09e0b1882e539e
TaherCCG <taher.ccg@gmail.com>
2024-12-18 18:15:20 +0000

    Add countdown and sum of even numbers example in task3.py. Day 5 of 100 Days of Python

---
+++ Day5/task3.py
@@ -0,0 +1,9 @@
+# Range
+for i in range(5, 0, -1):
+    print(i)
+
+# Sum of all even numbers from 1 to 100
+sum = 0
+for i in range(1, 101):
+    sum += i
+print(f"Sum of all numbers from 1 to 100: {sum}")
