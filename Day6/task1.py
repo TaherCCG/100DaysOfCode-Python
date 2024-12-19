67349dbd4bddc676ae11467a3ad6d5a18c809c62
TaherCCG <taher.ccg@gmail.com>
2024-12-19 18:28:16 +0000

    Add function examples in task1.py for greeting and user input.  Day 6 of 100 days

---
+++ Day6/task1.py
@@ -0,0 +1,10 @@
+def my_function():
+    print("Hello from a function")
+
+my_function()
+
+def get_user_name():
+    name = input("What is your name? ")
+    print(f"Hello {name}")
+
+get_user_name()
