77ecd1f9be493f30ce1bc29d2e3c89e8a5213831
TaherCCG <taher.ccg@gmail.com>
2024-12-21 19:07:34 +0000

    Add: Implement greet_with_name function in task2.py for Day 8 of 100 Days

---
+++ Day8/task2.py
@@ -0,0 +1,10 @@
+# Functions with input
+
+def greet_with_name(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+
+greet_with_name("Jack Bauer", "birmingham" )
+
+greet_with_name(location="London", name="Angela")
