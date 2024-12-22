e9ac23b068bc13941199ea19cd2cb733180fcf7b
TaherCCG <taher.ccg@gmail.com>
2024-12-22 11:42:54 +0000

    Add: Implement grading program to convert student scores to grades.

---
+++ Day9/ex9-grading-program.py
@@ -0,0 +1,25 @@
+# Grading Program
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+# Create an empty dictionary for the grades
+student_grades = {}
+
+# Convert scores to grades
+for student, score in student_scores.items():
+    if 91 <= score <= 100:
+        student_grades[student] = "Outstanding"
+    elif 81 <= score <= 90:
+        student_grades[student] = "Exceeds Expectations"
+    elif 71 <= score <= 80:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+# Print the student_grades dictionary
+print(student_grades)
