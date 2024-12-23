9a461c7b48631b5114afa2183ae8b2041c7d7a96
TaherCCG <taher.ccg@gmail.com>
2024-12-23 20:15:11 +0000

    Add: Implement format_name function to format first and last names

---
+++ Day10/task1.py
@@ -0,0 +1,8 @@
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name."""
+    f_name = f_name.title()
+    l_name = l_name.title()
+    return f"{f_name} {l_name}"
+
+print(format_name("tAher", "MAHMOOD"))
+
