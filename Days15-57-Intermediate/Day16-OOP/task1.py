a9babdbdaf18d6ec443d7f5892fcef468462a8ee
TaherCCG <taher.ccg@gmail.com>
2024-12-30 17:18:48 +0000

    Add: Create a new Turtle Graphics example in task1.py

---
+++ Days15-57-Intermediate/Day16-OOP/task1.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+print(tim)
+tim.color("dark orchid")
+tim.shape("turtle")
+tim.forward(100)
+
+screen = Screen()
+print(screen.canvheight)
+screen.exitonclick()
